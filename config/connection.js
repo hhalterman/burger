c4a4c8d43f5498426d2c4d4c48ed6c1136b06ff2
hhalterman <hhalter@bgsu.edu>
2019-02-11 14:36:34 -0500

    small error

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 
 // Set up our connection information
-const connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
