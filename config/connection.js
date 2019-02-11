ee75ce8e0edeac180a2a20cf8808dd67ac3b70a3
hhalterman <hhalter@bgsu.edu>
2019-02-11 13:46:10 -0500

    readme updated and jawsdb added

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@
 const mysql = require("mysql");
 
 // Set up our connection information
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Middlebass!6",
-  database: "burgers_db"
-});
+const connection;
+
+if (process.env.JAWSDB_URL) {
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+	connection = mysql.createConnection( {
+		host: 'localhost',
+		user: 'root',
+		password: 'Middlebass!6',
+		database: 'burgers_db'
+	});
+};
 
 
 connection.connect(function(err) {
